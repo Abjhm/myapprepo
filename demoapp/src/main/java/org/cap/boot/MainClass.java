972971da25d0af49d8cb0e940399b6c22b5f84bc
Abjhm <asaikumar456@gmail.com>
2021-03-09 07:50:44 +0530

    First commit on March 09 07.49am

---
+++ demoapp/src/main/java/org/cap/boot/MainClass.java
@@ -0,0 +1,13 @@
+package org.cap.boot;
+
+public class MainClass {
+	
+	public static void main(String[] args){
+		
+		int[] myarr = {12,45,15,85,58};
+		
+		for(int n:myarr)
+			System.out.println(n);
+	}
+
+}
